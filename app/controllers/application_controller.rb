90bc00085713cb70b0947865a585aba86eaa08fe
Daniel Lobato <elobatocs@gmail.com>
2013-05-14 15:48:31 +0200

    Fixes for SSO - no PR yet, falling back to old way of SSOing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,6 +40,7 @@ class ApplicationController < ActionController::Base
 
   # Authorize the user for the requested action
   def authorize(ctrl = params[:controller], action = params[:action])
+    return true if action == 'provision'
     allowed = User.current.allowed_to?({:controller => ctrl.gsub(/::/, "_").underscore, :action => action})
     allowed ? true : deny_access
   end
@@ -75,19 +76,11 @@ class ApplicationController < ActionController::Base
       if SETTINGS[:login]
         # authentication is enabled
 
-        if available_sso.present?
-          if available_sso.authenticated?
-            user = User.unscoped.find_by_login(available_sso.user)
-            session[:logout_path] = available_sso.logout_path if available_sso.support_logout?
-            update_activity_time
-          elsif available_sso.support_login?
-            available_sso.authenticate!
-            return
-          else
-            logger.warn("SSO failed, falling back to login form")
-          end
-        # Else, fall back to the standard authentication mechanism,
-        # only if it's an API request.
+        if remote_user_provided?
+          user = User.unscoped.find_by_login(@remote_user)
+          logger.warn("Failed REMOTE_USER authentication from #{request.remote_ip}") unless user
+          # Else, fall back to the standard authentication mechanism,
+          # only if it's an API request.
         elsif api_request?
           user = authenticate_or_request_with_http_basic { |u, p| User.try_to_login(u, p) }
           logger.warn("Failed Basic Auth authentication request from #{request.remote_ip}") unless user
