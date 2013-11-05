3320448388069a6df2699ec87cbb4a5283685ddd
Gavin McCance <gavin.mccance@cern.ch>
2013-11-05 09:39:50 +0100

    Remove redundant schema upgrade - already added in 20121210214810

--- db/migrate/20130506165002_add_subscribe_to_all_hostgroups_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddSubscribeToAllHostgroupsToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :subscribe_to_all_hostgroups, :boolean unless column_exists? :users, :subscribe_to_all_hostgroups
-  end
-
-  def self.down
-    remove_column :users, :subscribe_to_all_hostgroups if column_exists? :users, :subscribe_to_all_hostgroups 
-  end
-end
